2dedc93f45e6f631a253c69fa172f93c1013f97e
JasonYang916 <jyang142@fordham.edu>
2022-06-09 17:05:17 -0400

    exercise 1.14

---
+++ Part 1/exercise_1.14/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const Max = ({votes, anecdotes}) => {
+  let max = 0;  
+  for (let i = 0; i < anecdotes.length; i++){
+    if(votes[i] > votes[max])
+      max = i;    
+  }
+
+  return(
+    <div>
+      {anecdotes[max]} <br/>
+      <p>has {votes[max]} votes </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+
+  const Select = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  const Vote = () => {
+    const copy = [...points]
+    copy[selected]++
+    setPoints(copy)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]} <br/>
+      <p>has {points[selected]} votes </p>
+      <button onClick={Vote}>vote</button>
+      <button onClick={Select}>next anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      <Max votes={points} anecdotes={anecdotes}/>
+    </div>
+  )
+}
+
+export default App
