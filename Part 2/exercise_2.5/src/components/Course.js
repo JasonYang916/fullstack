53df61659e9ecfa7d16f98011bf9c380e71935c5
JasonYang916 <jyang142@fordham.edu>
2022-06-10 00:16:42 -0400

    Exercise 2.5

---
+++ Part 2/exercise_2.5/src/components/Course.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+    return (
+      <div>
+        <h2>{course.name}</h2>
+        {course.parts.map(part => 
+          <p key = {part.id}>{part.name} {part.exercises}</p>
+        )}
+        <p>
+          total of {course.parts.reduce((sum, part) => sum + part.exercises,0)} exercises
+        </p>
+      </div>
+    )
+}
+
+export default Course
