43420fc90ee578c131f0a195ce7ff05f4a64dea2
JasonYang916 <jyang142@fordham.edu>
2022-06-20 23:48:03 -0400

    Exercise 2.19

---
+++ Part 2/exercise_2.19/src/components/Filter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Filter = (props) => {
+    return(
+      <form onSubmit={props.func}>
+        <div>
+          filter shown with: <input
+            value={props.filter}
+            onChange={props.handler}
+          />
+        </div>
+      </form>
+    )
+  }
+
+  export default Filter
