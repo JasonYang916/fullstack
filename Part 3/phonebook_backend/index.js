483e467cb1226764ad1252c30913b1a4fa65963e
JasonYang916 <jyang142@fordham.edu>
2022-07-02 17:27:04 -0400

    Exercise 3.6

--- Part 3/phonebook_backend/index.js
+++ Part 3/phonebook_backend/index.js
@@ -61,6 +61,42 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1000) + 1
+}
+
+app.post('/api/persons', (request,response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number){
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())){
+    return response.status(409).json({
+      error: 'name must be unique'
+    })
+  }
+  
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
