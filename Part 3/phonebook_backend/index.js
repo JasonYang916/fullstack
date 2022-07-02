dd3b11131a69b00f611b6ce5b6be86dcfb6cfe2a
JasonYang916 <jyang142@fordham.edu>
2022-07-02 16:36:20 -0400

    Exercise 3.4

--- Part 3/phonebook_backend/index.js
+++ Part 3/phonebook_backend/index.js
@@ -40,7 +40,7 @@ app.get('/info', (request, response) => {
   )
 })
 
-//display parrticular person if found
+//display particular person
 //returns 404 otherwise.
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -50,6 +50,15 @@ app.get('/api/persons/:id', (request, response) => {
     response.json(person)
   else
     response.status(404).end()
+})
+
+//delete particular person
+//no need for test case if person does not exist, works fine
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
